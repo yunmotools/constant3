4c3ff7b36e5e1f307064fd32731591d1450439e0
jordonjones <jordon01jones@gmail.com>
2025-12-05 09:33:28 -0600

    constant 3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
-// Create constants to represent RGB color values for red, green, blue, white, and black.
+// Create constants to represent RGB color values for
+// red, green, blue, white, and black.
 
 public class Main {
+    private static final int RED = 0xFF0000;
+    private static final int GREEN = 0x00FF00;
+    private static final int BLUE = 0x0000FF;
+    private static final int WHITE = 0xFFFFFF;
+    private static final int BLACK = 0x000000;
+
+
     public static void main(String[] args) {
 
     }
